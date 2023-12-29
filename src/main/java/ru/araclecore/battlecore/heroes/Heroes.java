6e7c01cd092d89656621921e2b82d1e856547052
aracle <aracle.core@gmail.com>
2023-12-29 14:18:32 +0300

    Initial commit

---
+++ src/main/java/ru/araclecore/battlecore/heroes/Heroes.java
@@ -0,0 +1,17 @@
+package ru.araclecore.battlecore.heroes;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Heroes extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
